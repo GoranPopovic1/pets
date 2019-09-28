a3023ad896f6440348eff229ea5994d2c4a15661
Farscape <geoligard@gmail.com>
2019-09-28 19:44:26 +0200

    Add Ads Tables

---
+++ database/migrations/2019_09_28_170415_create_ads_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
+            $table->unsignedBigInteger('image_id');
+            $table->timestamps();
+
+            $table->foreign('image_id')->references('id')->on('ads_images')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
