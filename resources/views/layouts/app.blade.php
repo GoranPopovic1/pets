e25faa6d7e7376cb82e71a3b762565a2f5f9aabb
Farscape <geoligard@gmail.com>
2019-09-29 23:31:04 +0200

    Reorganize layuts and links

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,17 +35,6 @@
                     <ul class="navbar-nav mr-auto">
 
                     </ul>
-
-                    @auth
-                    <a href="{{ url('/postavi-oglas') }}">Postavi oglas</a>
-                    <a href="{{ url('/profile') }}">Profile</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                    @endif
-                    @endauth
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
@@ -59,13 +48,19 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">{{ __('Postavi oglas') }}</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}">
+                                    <a class="dropdown-item" href="{{ route('profile') }}">
+                                        {{ __('Profil') }}
+                                    </a>
+                                    <a class="dropdown-item" href="#">
                                         {{ __('Postavi oglas') }}
                                     </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
