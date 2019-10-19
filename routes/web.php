77302bf7892be685d74db094351e0b4f782ce268
Farscape <geoligard@gmail.com>
2019-10-19 13:50:01 +0200

    - Test

--- routes/web.php
+++ routes/web.php
@@ -56,3 +56,5 @@ Route::get('redirect/{driver}', 'Auth\LoginController@redirectToProvider')
 Route::get('{driver}/callback', 'Auth\LoginController@handleProviderCallback')
     ->name('login.callback')
     ->where('driver', implode('|', config('auth.socialite.drivers')));
+
+//test
